76539957a473a84b6eb70f99e85f86c09bf17894
bjucs <bju@umd.edu>
2025-01-28 16:30:14 +0100

    feat: redefine AddAssignment to more suitably interface with future API calls

--- todo.go
+++ todo.go
@@ -1,25 +1,42 @@
 package go_sheets
 
 import (
+	"fmt"
 	"time"
 )
 
+const (
+	// Format for inputted date strings is `MM/DD/YY`
+	DateFormat = "01/02/06"
+)
+
 type AssignmentItem struct {
-	Name        string
-	Completed   bool
-	CreatedAt   time.Time
-	CompletedAt time.Time
+	Name  string
+	Info  *string
+	DueAt time.Time
 }
 
 type List []AssignmentItem
 
 // Callable methods for go-sheets
-func (l *List) AddAssignment(name string) {
+func (l *List) AddAssignment(name string, due string, info ...string) {
+	dueDate, err := time.Parse(DateFormat, due)
+	if err != nil {
+		fmt.Println("Invalid date passed in (please use mm/dd/yy):", err)
+		return
+	}
+
+	var infoPtr *string
+	if len(info) == 0 || len(info) > 1 {
+		// Invalid amount of info passed in
+	} else if len(info) == 1 {
+		infoPtr = &info[0]
+	}
+
 	t := AssignmentItem{
-		Name:        name,
-		Completed:   false,
-		CreatedAt:   time.Now(),
-		CompletedAt: time.Time{},
+		Name:  name,
+		Info:  infoPtr,
+		DueAt: dueDate,
 	}
 
 	*l = append(*l, t)
--- todo_test.go
+++ todo_test.go
@@ -1,33 +1,45 @@
 package go_sheets
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestAddAssignment_SimpleAdd_Success(t *testing.T) {
 	l := List{}
 
 	taskName := "New Task"
-	l.AddAssignment(taskName)
+	dueDate := "02/02/25"
+	taskInfo := "Some new task"
+	l.AddAssignment(taskName, dueDate, taskInfo)
 
-	if l[0].Name != taskName {
-		t.Errorf("Expected %q, got %q instead", taskName, l[0].Name)
-	}
+	assert.Equal(t, l[0].Name, taskName)
+	assert.Equal(t, getDateFromTime(l[0].DueAt), dueDate)
+	assert.Equal(t, *l[0].Info, taskInfo)
+
 }
 
-func TestAddAssignment_MultipleAdds_Success(t *testing.T) {
-	l := List{}
-
-	taskName1 := "New Task 1"
-	taskName2 := "New Task 2"
-	taskName3 := "New Task 3"
-	l.AddAssignment(taskName1)
-	l.AddAssignment(taskName2)
-	l.AddAssignment(taskName3)
-
-	expectedTasks := []string{taskName1, taskName2, taskName3}
-
-	for i, task := range l {
-		if task.Name != expectedTasks[i] {
-			t.Errorf("Task at index %d: Expected %q, got %q instead", i, expectedTasks[i], task.Name)
-		}
-	}
+func getDateFromTime(t time.Time) string {
+	return t.Format(DateFormat)
 }
+
+// func TestAddAssignment_MultipleAdds_Success(t *testing.T) {
+// 	l := List{}
+
+// 	taskName1 := "New Task 1"
+// 	taskName2 := "New Task 2"
+// 	taskName3 := "New Task 3"
+// 	l.AddAssignment(taskName1)
+// 	l.AddAssignment(taskName2)
+// 	l.AddAssignment(taskName3)
+
+// 	expectedTasks := []string{taskName1, taskName2, taskName3}
+
+// 	for i, task := range l {
+// 		if task.Name != expectedTasks[i] {
+// 			t.Errorf("Task at index %d: Expected %q, got %q instead", i, expectedTasks[i], task.Name)
+// 		}
+// 	}
+// }
